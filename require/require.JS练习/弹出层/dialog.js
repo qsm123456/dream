5d6d8d59ee4084ee50a5477187f6b2758636a253
qsm123456 <2206652863@qq.com>
2017-12-04 22:32:28 +0800

    弹出层

---
+++ require/require.JS练习/弹出层/dialog.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ASUS1 on 2017/12/3.
+ */
+define(['jquery'],function ($) {
+    return{
+        open:function (settings) {
+            var defaultSettings = {
+                width: 500,
+                height: 400,
+                title: '弹出层',
+                content: ''
+            };
+            $.extend(defaultSettings, settings);
+            console.log(defaultSettings);
+            var html ='<div class="container">'
+                +'<div class="shadow">'
+                +'</div>'
+                +'<div class="box">'
+                +'<div class="title">'
+                +'<div class="item">'+defaultSettings.title+'</div>'
+                +'<div class="close">[X]</div>'
+                +'</div>'
+                +'<div class="content"></div>'
+                +'</div>'
+                +'</div>';
+            $(document.body).append(html);
+            $('.box').css({
+                width:defaultSettings.width,
+                height:defaultSettings.height
+            })
+
+        }
+    }
+})
+
+
+
+
---
+++ require/require.JS练习/弹出层/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by ASUS1 on 2017/12/4.
+ */
+require(['jquery','dialog'],function ($,dialog) {
+    $('#open').on('click',function () {
+        var settings = {
+            width:300,
+            height:200,
+            title:'注册',
+            content:''
+        };
+        dialog.open(settings);
+    })
+});
