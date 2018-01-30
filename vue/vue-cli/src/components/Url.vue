de578b2b5ec3c1473f791c4d97cef122001c9bcd
qsm123456 <2206652863@qq.com>
2018-01-30 22:42:12 +0800

    url传参数

---
+++ vue/vue-cli/src/components/Url.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <div>
+        <h5>这是测试URL传参的组件</h5>
+        <h3>用户ID：{{ $route.params.userId }}</h3>
+        <h3>用户名称：{{ $route.params.username }}</h3>
+        <router-view/>
+    </div>
+
+</template>
+
