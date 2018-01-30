81f58317fed98a012a8aa54603c9828d2f646a67
qsm123456 <2206652863@qq.com>
2018-01-30 22:49:14 +0800

    new Vue.Store()

---
+++ vue/vue-cli/src/vuex/store.js
@@ -0,0 +1,30 @@
+/**
+ * Created by ASUS1 on 2018/1/30.
+ */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const state = {
+  count:1,
+};
+
+const mutations = {
+  add(state,num){
+    state.count+=num;
+  },
+  reduce(state){
+    state.count--;
+  }
+};
+const getters = {
+  count: function (state) {
+    return state.count+=10;
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+})
