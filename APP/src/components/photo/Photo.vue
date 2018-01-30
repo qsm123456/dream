8c2a2458dedd3a520d5fea10f6ef13208c4c98c8
qsm123456 <2206652863@qq.com>
2018-01-30 22:30:31 +0800

    vueapp小例子的 单个页面

---
+++ APP/src/components/photo/Photo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <common-header bgcolor="rgb(63, 81, 181)">
+      <span>首页</span>
+      <h3 slot="title">Photo</h3>
+    </common-header>
+    <h1>图片</h1>
+    <common-footer bgcolor="rgb(63, 81, 181)">
+      <ul>
+        <li>电影</li>
+        <li>音乐</li>
+        <li>书籍</li>
+        <li>图片</li>
+      </ul>
+    </common-footer>
+  </div>
+</template>
+
+<script>
+  import CommonHeader from '@/components/common/Header';
+  import CommonFooter from '@/components/common/Footer';
+
+  export default {
+    components:{
+      CommonHeader,
+      CommonFooter,
+    }
+  }
+</script>
