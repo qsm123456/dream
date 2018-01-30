dd75c7f99a231c0d86c2188028b0eefdf92f7fac
qsm123456 <2206652863@qq.com>
2018-01-30 22:23:24 +0800

    vueapp小例子的 底部框架

---
+++ APP/src/components/common/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul class="footer" :style='{background:bgcolor}'>
+         <li><router-link to="/">电影</router-link></li>
+         <li><router-link to="/music">音乐</router-link></li>
+         <li><router-link to="/photo">书籍</router-link></li>
+         <li><router-link to="/book">图片</router-link></li>
+    </ul>
+
+</template>
+
+<script>
+  export default{
+      props:['bgcolor']
+  }
+</script>
+
+<style scoped>
+  .footer{
+    height: 1rem;
+    /*background-color: rgb(33, 150, 243);*/
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    line-height: 1rem;
+  }
+  .footer li{
+    flex-grow: 1;
+    text-align: center;
+    color: #ccc;
+  }
+  .footer li a{
+    color: #cccccc;
+  }
+  .footer li a.router-link-exact-active{
+    color: white;
+  }
+</style>
