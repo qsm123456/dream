580055c677b8a33d86aafd4181cba7d121de30aa
qsm123456 <2206652863@qq.com>
2018-01-30 22:36:45 +0800

    url传参数

---
+++ vue/vue-cli/src/components/Text1.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>子路由1</h1>
+        <h1>{{ $route.params.name }}</h1>
+        <h1>{{ $route.params.age }}</h1>
+        <router-view/>
+    </div>
+
+</template>
+<script>
+
+</script>
