f948d720a14d9f94620b3b2812704f88d2007b18
qsm123456 <2206652863@qq.com>
2018-01-30 22:30:17 +0800

    vueapp小例子的 单个页面

---
+++ APP/src/components/music/Music.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <common-header bgcolor='rgb(33, 150, 000)'>
+            <span>首页</span>
+            <h3 slot="title">Music</h3>
+        </common-header>
+
+        <common-footer bgcolor='rgb(33, 150, 000)'>
+            <ul>
+              <li>电影</li>
+              <li>音乐</li>
+              <li>书籍</li>
+              <li>图片</li>
+            </ul>
+        </common-footer>
+    </div>
+</template>
+
+<script>
+  import CommonHeader from '@/components/common/Header';
+  import CommonFooter from '@/components/common/Footer';
+
+  export default {
+    components:{
+      CommonHeader,
+      CommonFooter,
+    }
+  }
+</script>
+
+
+
+<style scoped>
+
+</style>
