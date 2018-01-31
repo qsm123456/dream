1786a0d4d1de265facfee07c6a69117137d0fd87
qsm123456 <2206652863@qq.com>
2018-01-31 23:56:41 +0800

    接口配置 ajax 请求

--- APP/src/components/movie/Movie.vue
+++ APP/src/components/movie/Movie.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
       <ul class="list" >
-      <li @click="getDetail(movie.id)" class='movie' v-for="movie in movieList" :key="movie.id">
-        <div class="movie-img">
-          <img :src="movie.img" alt="">
-        </div>
-      </li>
+          <li  class='movie' v-for="movie in movieList" >
+              <div class="movie-img">
+                <img :src="movie.img" alt="">
+              </div>
+              <div class="movie-detail">
+                <h3> {{movie.nm}} </h3>
+                <p>{{movie.ver}}</p>
+                <p>{{movie.star}}</p>
+                <p>{{movie.rt}}</p>
+              </div>
+          </li>
+        <div class="loading" v-show><img src="../../assets/img/loading.gif" alt=""></div>
+        <div class="tip" v-show>没有数据了</div>
     </ul>
   </div>
 </template>
@@ -15,32 +23,72 @@ import axios from 'axios';
 export default {
     data(){
         return {
-          movieList: [],
+            movieList:[],
+            isLoading:true,
+            isEnd:false,
         }
-  },
-    methods: {
-        getMovieData(){
-          axios
-            .get(
-              API_PROXY +
-              `http://m.maoyan.com/movie/list.json?type=hot&offset=${
-                this.movieList.length
-                }&limit=10`
-            );
-          },
-        getDetail(movieId) {
-        this.$router.push(`/moviedetail/${movieId}`);
-        },
-  },
-    created(){
-        this.getMovieData;
     },
+    methods:{
+       getData(){
+         axios
+           .get(API_PROXY +  `http://m.maoyan.com/movie/list.json?type=hot&offset=${this.movieList.length}&limit=10`)
+           .then(res =>{
+  //             console.log(res)
+             let list =  res.data.data.movies;
+             if(list<10){
+                this.isEnd=true;
+             }else{
+                this.movieList = this.movieList.concat(list);
+                this.isLoading = false;
+             }
+           }).catch(res=>{   alert('加载失败')})
+    },
+    },
+    created(){
+          this.getData();
+         },
+    mounted() {
+    window.onscroll = () => {
+      let scrollTop = document.documentElement.scrollTop;
+      let clientHeight = document.documentElement.clientHeight;
+      let scrollHeight = document.documentElement.scrollHeight;
+      if (scrollTop + clientHeight == scrollHeight && !this.isEnd) {
+        this.isLoading = true;
 
+      }
+    };
+  }
 }
 </script>
 
 
 
 <style scoped>
-csdru
+  .list{
+    margin: 1rem 0;
+  }
+  .movie{
+    display: flex;
+    margin: .03rem;
+    border-bottom: .03rem solid #ccc;
+  }
+  .movie-img{
+    flex-grow: 1;
+    width: 0;
+    padding: .1rem;
+}
+  .movie-detail{
+    flex-grow: 2;
+    width: 0;
+    text-align: left;
+  }
+.movie-detail h3{
+    font-weight: bolder;
+}
+  .loading{
+    position: fixed;
+    bottom: 1rem;
+    left: 50%;
+    transform: translate(-50%);
+  }
 </style>
