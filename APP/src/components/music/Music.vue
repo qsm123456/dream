47b06089ad19b26cee06bcabe31a32167e483dc1
qsm123456 <2206652863@qq.com>
2018-02-02 22:44:47 +0800

    单页面app 音乐

--- APP/src/components/music/Music.vue
+++ APP/src/components/music/Music.vue
@@ -1,31 +1,34 @@
 <template>
-
+    <div>
+      <ul class="music-menu">
+        <li class="newsong"><router-link to="/newsong">新歌</router-link></li>
+        <li class="ranking">排行</li>
+        <li class="playlist">歌单</li>
+        <li class="singer">歌手</li>
+      </ul>
+    </div>
 </template>
 
 <script>
-  import axios from 'axios';
-    export default{
-        data(){
-            return {
-              musicList:[]
-            }
-        },
-        created(){
-          axios
-            .post(API_PROXY + 'http://music.163.com/api/search/get/web?csrf_token=')
-            .then(res=>{
-                console.log(res);
-//               this.musicList =
-            })
-            .catch(res=>{
-                alert('出错了')
-            })
-        }
-    }
+export default{
+
+}
 </script>
 
 
 
 <style scoped>
-
+  .music-menu{
+    height: 1rem;
+    /*background: #b89f7a;*/
+    width: 100%;
+    display: flex;
+  }
+  .music-menu li{
+    float: left;
+    flex-grow: 1;
+    font-size: .4rem;
+    line-height: 1rem;
+    /*border-bottom: .1rem solid ;*/
+  }
 </style>
