c69d5aa075f99720884f58bc854517d1a31ba66f
qsm123456 <2206652863@qq.com>
2018-02-03 17:03:20 +0800

    单页面音乐详细页面

--- APP/src/components/music/Newsong.vue
+++ APP/src/components/music/Newsong.vue
@@ -1,27 +1,36 @@
 <template>
-  <div class="newsong">
-    <!--<div class="newsong-img"><img :src="pic.imgurl" alt=""></div>-->
-    <ul class="ns-list">
-      <li v-for="music in musicList">{{music.filename}}</li>
-    </ul>
-    <audio src=""></audio>
-  </div>
+      <!--<h1>{{$route.params.musicId}}</h1>-->
+      <aplay autoplay  :music='musicList' :showlrc="true" v-if="load" ></aplay>
+
 </template>
 
 <script>
+  import aplay from 'vue-aplayer'
   import axios from 'axios';
   export default{
+    components: {
+      aplay,
+    },
     data(){
       return {
         musicList:[],
+        load:false,
       }
     },
     created(){
       axios
-        .get(API_PROXY + 'http://m.kugou.com/?json=${20130320}')
+        .get('/static/data/musicdata.json')
         .then(res=>{
           console.log(res);
-          this.musicList = res.data.data;
+          res.data.musicData.forEach(element=>{
+              this.musicList.push(
+              {title : element.title,
+              author :element.author,
+              pic:element.musicImgSrc,
+              url:element.src,
+              lrc : '/static/'+element.lrc})
+          }),
+            this.load = true
         })
         .catch(res=>{
           alert('出错了')
@@ -33,12 +42,7 @@
 
 
 <style scoped>
-  .ns-list{
-    margin-left: 1rem;
-  }
-.ns-list li {
-  height: .7rem;
-  text-align: left;
-  /*text-indent: 1rem;*/
-}
+  .aplayer.content.aplayer-withlist{
+   margin: 1rem 0;
+ }
 </style>
