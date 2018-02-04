fe57da2e73c11e714de8455e62fb8dac2f8f3033
qsm123456 <2206652863@qq.com>
2018-02-04 21:24:25 +0800

    vue 框架写的照片集

--- APP/src/components/photo/Photo.vue
+++ APP/src/components/photo/Photo.vue
@@ -1,29 +1,51 @@
 <template>
   <div>
-    <common-header bgcolor="rgb(63, 81, 181)">
-      <span>首页</span>
-      <h3 slot="title">Photo</h3>
-    </common-header>
-    <h1>图片</h1>
-    <common-footer bgcolor="rgb(63, 81, 181)">
-      <ul>
-        <li>电影</li>
-        <li>音乐</li>
-        <li>书籍</li>
-        <li>图片</li>
-      </ul>
-    </common-footer>
+    <ul class="piclist clearfix" >
+      <li class='photo' v-for="(item,index) in photoList" :key="index" >
+          <img @click="big(index)" :src="item.src" alt="">
+      </li>
+    </ul>
   </div>
+
 </template>
 
 <script>
-  import CommonHeader from '@/components/common/Header';
-  import CommonFooter from '@/components/common/Footer';
-
-  export default {
-    components:{
-      CommonHeader,
-      CommonFooter,
-    }
-  }
+  import axios from 'axios'
+ export default{
+    data(){
+        return{
+            photoList:[],
+        }
+    } ,
+    created(){
+       axios
+        .get('/static/data/photodata.json')
+        .then(res=>{
+//           console.log(res);
+          this.photoList = res.data.photoData;
+    })
+      .catch(res=>{
+        alert('出错了')
+      })
+    },
+   methods:{
+        big(photoIndex){
+            this.$router.push(`/photodetail/${photoIndex}`)
+        }
+   }
+ }
 </script>
+
+
+<style scoped>
+.clearfix::after{
+    content: '';
+    display: block;
+    clear: both;
+
+}
+  .photo{
+    width: 50%;
+    float: left;
+  }
+</style>
