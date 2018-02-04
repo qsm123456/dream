385edcecc6367547bf1fd9dd57c08163cc3e3370
qsm123456 <2206652863@qq.com>
2018-02-04 22:36:30 +0800

    vue 框架写的照片集

---
+++ APP/src/components/photo/PhotoDetail.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-touch  v-on:swipeleft="callbackl"  v-on:swiperight="callbackr" >
+      <div class="picDetail">
+        <img  :src="'/static/img1/' + nowIndex  + '.jpg'" alt="">
+      </div>
+    </v-touch>
+</template>
+
+<script>
+  import axios from 'axios'
+export default{
+    data(){
+        return {nowIndex:0};
+    },
+    created(){
+      this.nowIndex = parseInt(this.$route.params.photoIndex) + 1
+    },
+    methods:{
+      callbackl(){
+        this.nowIndex ++;
+//        console.log($route.params.photoIndex)
+        if(this.nowIndex == 24){
+          this.nowIndex=1
+        }
+      },
+      callbackr(){
+        this.nowIndex --;
+        if(this.nowIndex == 0){
+          this.nowIndex = 24;
+        }
+      },
+    }
+}
+</script>
+
+
+<style scoped>
+.picDetail{
+    position: absolute;
+    top:0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background: #000;
+}
+ img{
+    width: 100%;
+    position: absolute;
+    top:50%;
+    left: 0;
+    transform:translateY(-50%);
+ }
+</style>
