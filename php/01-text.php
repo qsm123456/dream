9ab6aa840d4087eb998d1f708c33ae71602c62f1
qsm123456 <2206652863@qq.com>
2018-03-10 22:43:45 +0800

    九九乘法表  php

---
+++ php/01-text.php
@@ -0,0 +1,18 @@
+<?php
+
+// echo '<table>';
+for($i=1;$i<=9;$i++){
+	// echo '<tr>';
+	for($j=1;$j<=$i;$j++){
+		// echo '<td>';
+		echo $i." * ".$j." = ".($i*$j).' ';
+		// echo "<br>";
+		// echo '</td>';
+	}
+	echo "<br>";
+	// echo '</tr>';
+	
+}
+// echo '</table>';
+
+?>
