21cac095e26d25b9d86787edf9ab3063e76716ad
qsm123456 <2206652863@qq.com>
2018-03-13 22:20:44 +0800

    分数查询 php

--- php/05-text.php
+++ php/05-text.php
@@ -2,9 +2,25 @@
 	if(isset($_POST['sub'])){
 		// $_POST()
 		// echo 123;
-		echo '<pre>';
-		var_dump($_POST);
-		echo '</pre>';
+		// echo '<pre>';
+		// var_dump($_POST);
+		// echo '</pre>';
+		if(isset($_POST['sub'])){
+			$score=$_POST['score'];
+			
+
+			if($score==100){
+				echo "<script>alert('优秀')</script>";
+			}else{
+				if($score==80){
+					echo "<script>alert('合格')</script>";
+				}else{
+					echo "<script>alert('不合格')</script>";
+				}
+			}
+
+
+		}
 		
 	}
 ?>
@@ -12,7 +28,7 @@
 	<form action='05-text.php' method='post'>
 	<h3>输入分数查询成绩<h3>
 	<input type='submit' name='sub' value='提交'>
-	<input type='text' name='content'>	
+	<input type='text' name='score'>	
 	<input type='submit' name='reset' value='重置'>
 	</form>
 	
