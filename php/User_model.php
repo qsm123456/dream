8f21958e05f31e31433d7ca299981d492897d6de
qsm123456 <2206652863@qq.com>
2018-03-31 23:54:30 +0800

    php =》model   处理逻辑代码 连接数据库

--- php/User_model.php
+++ php/User_model.php
@@ -2,24 +2,71 @@
 defined('BASEPATH')OR exit('NO direct script access allowed');
 
 
-class User_model extends CI_Model{
+class User_model extends CI_Model {
 	
-	public function save($username,$pwd,$realname,$tel){
-		$this -> db -> insert('t_user',array(
-			'username'=>$username,
-			'password'=>$pwd,
-			'realname'=>$realname,
-			'tel'=>$tel));
-		return $this -> db ->affected_rows();
+	public function find_by_email_pwd($email,$pwd){
+		return $this -> db -> get_where('t_user',array(
+			'email' =>$email,
+            'password' => $pwd
+		))->row();
 	}
-	
-	// public function find_by_name($username,$pwd){
-		// $this -> db -> get_where('t_user',array(
-			// 'username'=>$username,
-			// 'password'=>$pwd,
-		// ));
-		// return $query -> row();//返回一条记录
-	// }
-}
 
-?>
+	public function save($email,$pwd,$name,$sex){
+		$this -> db -> insert('t_user',array(
+			'email' =>$email,
+			'password' => $pwd,
+			'username' =>$name,
+			'sex' =>$sex,
+		));
+		return $this ->db->affected_rows();
+	}
+
+
+	public function find_by_email($email){
+		return $this -> db -> get_where('t_user',array(
+			'email' =>$email,
+		))->row();
+	}
+
+
+	public function find_type_by_user($user_id){
+		return $this -> db -> get_where('t_type_blog',array(
+			'user_id'=>$user_id
+		))->result();
+	}
+
+
+
+	//blog 模板
+	public function p_blog($title,$content,$type_id){
+		$this -> db -> insert('t_blog',array(
+			'title' =>$title,
+			'content' => $content,
+			'type_id' =>$type_id,
+		));
+		return $this ->db->affected_rows();
+	}
+
+
+	public function get_blog_count_by_user($user_id){
+		$this -> db -> select('b.*,bt.type_name');
+		$this -> db -> from('t_blog b');
+		$this -> db -> join('t_type_blog bt','b.type_id = bt.type_id');
+		$this -> db -> where('bt.user_id',$user_id);
+	    $user_blogs = $this ->db ->get();
+ 		return   $user_blogs->result();
+	}
+
+	public function get_blog_count_type_blog(){
+		$this -> db -> select('b.*');
+		$this -> db -> from('t_blog b');
+		$this -> db -> join('t_type_blog bt','b.type_id = bt.type_id');
+		$this -> db -> where('bt.user_id',$user_id);
+		$this -> db -> limit($num,$offset);
+	    $same_type_id = $this ->db ->get();
+ 		return   $same_type_id ->result();
+	}
+
+
+
+}
