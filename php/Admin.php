655748555e5303ec16b23002e3116799c5246355
qsm123456 <2206652863@qq.com>
2018-04-06 00:13:09 +0800

    php =》控制器admin

--- php/Admin.php
+++ php/Admin.php
@@ -9,11 +9,7 @@ class Admin extends CI_Controller {
 		}else{
 			redirect('welcome/login');
 		}
-	
 	}
-
-
-
 
 	public function  newblog(){
 		 
@@ -30,6 +26,9 @@ class Admin extends CI_Controller {
 	}
 
 
+	public function new_blog(){
+		$this -> load -> view('new_blog');
+	}	 
 
 	public function blog_comment(){
 		$this -> load -> view('blog_comments');
@@ -46,20 +45,21 @@ class Admin extends CI_Controller {
 		$this -> load ->model('user_model');
 
 		$user_blogs = $this-> user_model ->get_blog_count_by_user($user ->user_id);
-
 		$total_rows = $this -> user_model -> get_blog_id_total(
-			$user ->user_id,$this -> uri ->segment(3),$config['per_page']
+			$user ->user_id,$config['per_page'],$this -> uri ->segment(3)
 		);
 		$config['base_url'] = 'http://localhost/php/admin/index_logined';
-
 		$config['total_rows'] = count($user_blogs);
-		
+		$config['prev_link'] = '上一页';
+		$config['next_link'] = '下一页';
+		$config['first_link'] = '第一页';
+		$config['last_link'] = '最后一页';
+
 		$this->pagination->initialize($config);
-
 		$link = $this ->pagination ->create_links();
-
+		
 		$this -> load ->view('index_logined',array(
-			'user_blogs' =>$user_blogs,
+			'user_blogs' =>$total_rows,
 			'links'=>$link,
 		));
 	}
